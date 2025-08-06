342169c8fe40bb618fa7a2987c5f0842506707c4
Abhishek Karande <akarande70@gmail.com>
2025-08-06 12:33:07 +0000

    let's stick with theme A and remove  the toggle button

--- src/app/theme-provider.tsx
+++ src/app/theme-provider.tsx
@@ -6,5 +6,5 @@ import { ThemeProvider as NextThemesProvider } from "next-themes"
 import { type ThemeProviderProps } from "next-themes/dist/types"
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props} themes={['theme-a', 'theme-b']}>{children}</NextThemesProvider>
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
